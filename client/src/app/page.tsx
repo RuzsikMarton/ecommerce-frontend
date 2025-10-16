7c4dbd62ac7127e934e598b786bcc8fb7133a215
Márton Ruzsik <r.marton5@gmail.com>
2025-10-16 17:42:04 +0200

    finished: navbar footer and slider

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,12 @@
-import Image from "next/image";
+import Slide from "@/components/Slide";
 
 export default function Home() {
   return (
-    <div>Hello there</div>
+    <main className="relative">
+      <Slide />
+      <div className="mx-auto p-4 sm:max-w-xl md:max-w-2xl lg:max-w-5xl xl:max-w-7xl">
+        Content
+      </div>
+    </main>
   );
 }
